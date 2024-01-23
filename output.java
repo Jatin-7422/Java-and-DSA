4287ce96e0ea144d98a92f67d248defe1ba01789
Jatin Kumar <jatinkumar7422@gmail.com>
2024-01-23 10:49:00 +0530

    Outputs in JAVA

---
+++ output.java
@@ -0,0 +1,8 @@
+class output{
+    // void main  - function named as main and a return type void 
+    public static void main(String args[]){
+        System.out.print("Hello World\nthis is my first program \nand i am happy to say that it is run successfully");
+        System.out.print("\n*\n**\n***\n****");
+       
+    }
+} 
