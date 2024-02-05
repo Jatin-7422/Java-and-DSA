89387665811a09103c69a9f775e8cd06c21828ee
Jatin Kumar <jatinkumar7422@gmail.com>
2024-02-05 18:09:05 +0530

    Check Subsequences of a particular String

--- Problems/Subsequences.java
+++ Problems/Subsequences.java
@@ -1,25 +1,24 @@
 package Problems;
 
 public class Subsequences {
-    
-    public static void subsequences(String str , int idx, String NewString){
+
+    public static void subsequences(String str, int idx, String NewString) {
         if (idx == str.length()) {
             System.out.println(NewString);
             return;
         }
-        
+
         char CurrentChar = str.charAt(idx);
 
         // to be in a new string
         subsequences(str, idx + 1, NewString + CurrentChar);
-
-
+        
         // Not to be in a new string
-        subsequences(str, idx+1, NewString);
+        subsequences(str, idx + 1, NewString);
     }
-    
+
     public static void main(String[] args) {
         String str = "abc";
-        subsequences(str, 0, ""); 
+        subsequences(str, 0, "");
     }
 }
