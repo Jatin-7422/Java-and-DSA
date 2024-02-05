57c3c2f51a4a5a1ed308bfb7f601e955c188bcdc
Jatin Kumar <jatinkumar7422@gmail.com>
2024-02-05 18:07:47 +0530

    Check wheather the particular Array is sorted in a particular increasing manner

--- Problems/Strictly_Increasing.java
+++ Problems/Strictly_Increasing.java
@@ -1,23 +1,24 @@
 package Problems;
 
 public class Strictly_Increasing {
-    
-    public static boolean ArrayOrderedStrictly(int arr[] , int idx) {
-        
-        if (idx == arr.length-1) {
+
+    public static boolean ArrayOrderedStrictly(int arr[], int idx) {
+
+        if (idx == arr.length - 1) {
             return true;
         }
-        
-        if (arr[idx] < arr[idx+1]) {
-            //  Array is sorted 
-            return ArrayOrderedStrictly(arr, idx+1);
-        }else{
+
+        if (arr[idx] < arr[idx + 1]) {
+            // Array is sorted
+            return ArrayOrderedStrictly(arr, idx + 1);
+        } else {
+            // Array is not Sorted
             return false;
         }
     }
-    
+
     public static void main(String[] args) {
-        int arr[] = {1,3,5 ,2};
+        int arr[] = { 1, 3, 5, 2 };
         System.out.println(ArrayOrderedStrictly(arr, 0));
     }
 }
