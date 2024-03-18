a91a6a0821449eeb1a761bea5c75aeaf0f102152
Jatin Kumar <jatinkumar7422@gmail.com>
2024-03-18 06:55:36 +0530

    Quick Sort in Java

---
+++ Problems/Quick_Sort.java
@@ -0,0 +1,54 @@
+package Problems;
+
+public class Quick_Sort {
+
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high]; // Choose last element as pivot
+        int i = low - 1; // Index of smaller element
+
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            // pi is partitioning index, arr[pi] is now at right place
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+// Time Complexity --> O(n^2) --> Worst Case
+// Time Complexity --> O(nlogn) --> Average Case
